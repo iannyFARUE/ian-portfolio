4e9db1c14aa4d5d7b5955f9d0b1ff1832aec711d
Ian Madhara <iannyfarai@gmail.com>
2025-01-02 23:37:31 -0500

    added navbar markup

--- src/pages/navbar/Navbar.jsx
+++ src/pages/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Navbar.css";
+import Logo from "../../assets/logo.jpg";
+import menuItems from "./menu";
+import { IoIosColorPalette } from "react-icons/io";
 
 function Navbar() {
   return (
     <nav>
-      <h2>Navbar</h2>
+      <div className="container nav__container">
+        <a href="">
+          <img src={Logo} alt="Logo" />
+        </a>
+        <ul className="nav__menu">
+          {menuItems.map((menu) => (
+            <li key={menu.id}>
+              <a href={menu.link}>{menu.title}</a>
+            </li>
+          ))}
+        </ul>
+        <button id="theme__icon">
+          <IoIosColorPalette />
+        </button>
+      </div>
     </nav>
   );
 }

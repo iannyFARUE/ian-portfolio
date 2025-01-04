f80a370029130be59372e2b6cefc01cf5fb1d587
Ian Madhara <iannyfarai@gmail.com>
2025-01-03 22:38:41 -0500

    added styles for header

--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-import HeaderImage from "../../assets/header.jpg";
+import HeaderImage from "../../assets/ian.jpeg";
 import SocialIcons from "./SocialIcons";
 function Header() {
   return (
@@ -9,16 +9,18 @@ function Header() {
         <div className="header__profile">
           <img src={HeaderImage} alt="Header Portrait" />
         </div>
-        <h3>Ian Madhara</h3>
+        <h3>Hello, I 'm Ian Madhara</h3>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
-          nesciunt eius eaque, cumque iste praesentium quidem optio suscipit
-          delectus qui dolore eum recusandae consectetur officiis ut sapiente
-          possimus repudiandae cupiditate.
+          I'm an AI/ML engineer
+          <span>Please </span>
         </p>
         <div className="header__cta">
-          <a href="#contact">Let's Talk</a>
-          <a href="#portfolio">My Work</a>
+          <a href="#contact" className="btn primary">
+            Let's Talk
+          </a>
+          <a href="#portfolio" className="btn light">
+            My Work
+          </a>
         </div>
         <SocialIcons />
       </div>
--- src/pages/navbar/menu.js
+++ src/pages/navbar/menu.js
@@ -1,10 +1,12 @@
-
 const data = [
-    {id: 1, link: '#', title: 'Home'},
-    {id: 2, link: '#about', title: 'About'},
-    {id: 3, link: '#services', title: 'Services'},
-    {id: 4, link: '#portfolio', title: 'Portfolio'},
-    {id: 5, link: '#contact', title: 'Contact'}
-    ]
+  { id: 1, link: "#", title: "Home" },
+  { id: 2, link: "#about", title: "About" },
+  { id: 3, link: "#education", title: "Education" },
+  { id: 4, link: "#portfolio", title: "Portfolio" },
+  { id: 5, link: "#certifications", title: "Certifications" },
+  { id: 6, link: "#skills", title: "Skills" },
+  { id: 7, link: "#projects", title: "Projects" },
+  { id: 8, link: "#contact", title: "Contact" },
+];
 
-    export default data;
+export default data;

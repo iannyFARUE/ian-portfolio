e5ad96a0625b0df110df108f4a61e70132d530c0
Ian Madhara <iannyfarai@gmail.com>
2025-03-01 11:19:18 -0500

    added faq

--- src/pages/faqs/Faqs.jsx
+++ src/pages/faqs/Faqs.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import faqs from "./data";
+import Faq from "./Faq";
 import "./Faqs.css";
 
-function Faqs() {
+const FAQs = () => {
   return (
     <section id="faqs">
-      <h2>FAQS</h2>
+      <h2>Frequently Asked Questions</h2>
+      <p>
+        Here are some questions I usually get. Click to toggle the answer, and
+        if you still have some more questions, shoot me a message from the
+        contact section!
+      </p>
+      <div className="container faqs__container">
+        {faqs.map((faq) => (
+          <Faq key={faq.id} faq={faq} />
+        ))}
+      </div>
     </section>
   );
-}
+};
 
-export default Faqs;
+export default FAQs;
